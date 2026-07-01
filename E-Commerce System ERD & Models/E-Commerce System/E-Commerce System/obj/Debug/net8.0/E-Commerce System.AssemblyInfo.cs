0d171d59855edd39c83b8e88b636b44d6fc4b227
“Anoud <anoudalsaidi1993@gmail.com>
2026-07-01 13:31:22 +0400

    Create Models folder structure

--- E-Commerce System ERD & Models/E-Commerce System/E-Commerce System/obj/Debug/net8.0/E-Commerce System.AssemblyInfo.cs
+++ E-Commerce System ERD & Models/E-Commerce System/E-Commerce System/obj/Debug/net8.0/E-Commerce System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a278f39f87923e880119a83b876ae5e1f1a58f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a4612db3972cf4a100200fbd30dff2ef01494d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
