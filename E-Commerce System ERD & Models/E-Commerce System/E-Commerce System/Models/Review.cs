f9942752fd99bd7f85d54816bd6f72846ba4c213
“Anoud <anoudalsaidi1993@gmail.com>
2026-07-01 13:52:39 +0400

    Finalize model layer based on project requirements

--- E-Commerce System ERD & Models/E-Commerce System/E-Commerce System/Models/Review.cs
+++ E-Commerce System ERD & Models/E-Commerce System/E-Commerce System/Models/Review.cs
@@ -9,7 +9,10 @@ namespace E_Commerce_System.Models
    public class Review
     {
         public int reviewId { get; set; }
-
+        public int userId { get; set; }
+        public int productId { get; set; }
+        public int rating { get; set; }
         public string comment { get; set; }
+        public DateTime reviewDate { get; set; }
     }
 }
